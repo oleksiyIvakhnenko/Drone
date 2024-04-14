3cfdc6e23985d2b370c05a42f7b83954008cddcf
Ivakhnenko <ivakhnenko.aleksi@gmail.com>
2024-04-14 15:48:32 +0300

    base functionality

--- ANanoTx/src/main.cpp
+++ ANanoTx/src/main.cpp
@@ -1,18 +1,34 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <RF24.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// CE	6 white
+// CSN	7 yellow
+// SCK	13 green
+// MOSI	11 blue
+// MISO	12 pink
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+RF24 radio(6, 7); // Підключення пінів CE та CSN до 6 і 7 відповідно
+
+byte joystickX = A0;
+int xValue;
+
+void setup()
+{
+  Serial.begin(9600);
+  radio.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  String str = "joystickX=" + String(analogRead(joystickX));
+  int str_len = str.length() + 1;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println(str);
+
+
+  char text[str_len];
+  str.toCharArray(text, str_len);
+
+  radio.openWritingPipe(0xF0F0F0F0E1LL); // Встановлення адреси отримувача
+  radio.write(&text, sizeof(text));      // Відправлення даних по радіоканалу
 }
--- AUnoRx/src/main.cpp
+++ AUnoRx/src/main.cpp
@@ -1,18 +1,63 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SPI.h>
+#include <RF24.h>
+#include <TM1637.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void displayNumber(int num);
+
+TM1637 tm(2, 3);
+
+// CE 6
+// CSN  7
+// SCK  13
+// MOSI 11
+// MISO 12
+
+RF24 radio(6, 7); // Підключення пінів CE та CSN до 6 і 7 відповідно
+
+int pValue = 0;
+
+void setup()
+{
+
+  radio.begin();
+
+  tm.init();
+  tm.set(4);
+
+  radio.openReadingPipe(1, 0xF0F0F0F0E1LL); // Встановлення такої самої адреси, як і на передавачі
+  radio.startListening();                   // Перехід в режим прийому
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  String sString;
+  if (radio.available())
+  {
+    char text[32] = "";
+    radio.read(&text, sizeof(text)); // Зчитування отриманих даних
+
+    sString = String(text);
+    size_t found = sString.indexOf("=");
+
+    String key = sString.substring(0, found);    // get first part
+    String value = sString.substring(found + 1); // get second part
+
+    if (key == "joystickX")
+    {
+      pValue = atoi(value.c_str());
+    }
+  }
+
+  displayNumber(pValue);
+
+  // displayNumber(125);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void displayNumber(int num)
+{
+  tm.display(3, num % 10);
+  tm.display(2, num / 10 % 10);
+  tm.display(1, num / 100 % 10);
+  tm.display(0, num / 1000 % 10);
 }
